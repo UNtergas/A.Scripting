6b4ef3f4d10b2d38a47577816b503a406c929017
UNtergas <mycomputerer135@gmail.com>
2022-03-07 08:59:17 +0700

    f

---
+++ 1r-Pt.py
@@ -0,0 +1,15 @@
+import math
+radians = 0.7
+height =  math.sin(radians)
+def ngu (a):
+    a=a*2
+    print(a)
+
+ngu(4)
+
+import turtle
+bob = turtle.Turtle()
+bob.fd(100)
+bob.lt(200)
+bob.fd(200)
+turtle.mainloop()
