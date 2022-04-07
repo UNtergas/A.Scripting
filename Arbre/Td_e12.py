b98c54d2d2cf8f5408a4b578d6abf5613c6fa525
UNtergas <mycomputerer135@gmail.com>
2022-04-07 16:09:16 +0700

    fini ex5(cheminement) + add func compté nombre feuille)

--- Arbre/Td_e12.py
+++ Arbre/Td_e12.py
@@ -17,24 +17,38 @@ class Fibo():
     #
     def hauter(tree):
         if not tree:
-            return 0
+            return -1
         return max(Fibo.hauter(tree.fdro),Fibo.hauter(tree.fgau)) + 1
     #
-    def nombre_feuille(tree):
+    def nombre_noeud(tree):
         if tree is None:
             return 0
         else:
-            return Fibo.nombre_feuille(tree.fdro) + Fibo.nombre_feuille(tree.fgau) + 1
+            return Fibo.nombre_noeud(tree.fdro) + Fibo.nombre_noeud(tree.fgau) + 1
 
-    def cheminement(tree):
-        if tree.fdro != None and tree.fgau != None:     
-            return Fibo.cheminement(tree.fdro)+Fibo.cheminement(tree.fgau)+1
-        return 1
+    def nombre_feuille(tree,count=0):
+        if tree.fdro == None and tree.fgau != None:     
+            return Fibo.nombre_feuille(tree.fgau,count)
+        elif tree.fdro != None and tree.fgau == None:
+            return Fibo.nombre_feuille(tree.fdro,count)
+        elif tree.fdro == None and tree.fgau == None:
+            return count+1
+        return Fibo.nombre_feuille(tree.fdro,count)+Fibo.nombre_feuille(tree.fgau,count)
+        
+
+    def cheminement(tree,taille=0):
+        if tree.fdro == None and tree.fgau != None:     
+            return Fibo.cheminement(tree.fgau,taille+1)
+        elif tree.fdro != None and tree.fgau == None:
+            return Fibo.cheminement(tree.fdro,taille+1)
+        elif tree.fdro == None and tree.fgau == None:
+            return taille
+        return Fibo.cheminement(tree.fdro,taille+1)+Fibo.cheminement(tree.fgau,taille+1)
     #
 if __name__=='__main__':
-    root=noeud(4)
+    root=noeud(5)
     Fibo.creeeFibonaci(root)
     e6.afficher(root,-1)
-    print(f"nombre feuille est: {Fibo.nombre_feuille(root)} et arbre taille: {Fibo.hauter(root)}")
+    print(f"nombre node est: {Fibo.nombre_noeud(root)} nombre feuille est : {Fibo.nombre_feuille(root)} et arbre taille: {Fibo.hauter(root)}")
     print(f"cheminement:{Fibo.cheminement(root)}")
     
